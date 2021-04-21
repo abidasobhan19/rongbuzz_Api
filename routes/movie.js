0f49015553bc8dad5fcd93b47235293ac36ed271
abidasobhan19 <abidasobhan19@gmail.com>
2021-04-21 13:57:50 +0600

    04/21/21

--- routes/movie.js
+++ routes/movie.js
@@ -7,11 +7,28 @@ const router = express.Router()
 
 router.get('/',(req , res)=>{
     Movie.find()
-    .limit(500)
+    .limit(200)
     .then(movies=>res.status(200).json(movies))
     .catch(error=>res.status(500).json(error))
     
 })
+
+
+router.get('/popular',(req , res)=>{
+    Movie.find({is_popular:true})
+    .populate('category')
+    .then(movies=>res.status(200).json(movies))
+    .catch(error=>res.status(500).json(error))
+})
+
+router.get('/featured',(req , res)=>{
+    Movie.find({is_featured:true})
+    .populate('category')
+    .then(movies=>res.status(200).json(movies))
+    .catch(error=>res.status(500).json(error))
+})
+
+
 
 
 router.post('/',(req,res)=>{
